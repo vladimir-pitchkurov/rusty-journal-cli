6943466da1edd69541337dbc3dd847d8d7c92bc8
volodimir-pichkurov <vladimirpitbul@gmail.com>
2023-04-27 21:46:02 +0300

    created tasks module v1.2

--- src/tasks.rs
+++ src/tasks.rs
@@ -3,9 +3,8 @@ use serde::Deserialize;
 use serde::Serialize;
 use std::fs::{File, OpenOptions};
 use std::path::PathBuf;
-use std::io::{Result, Seek, SeekFrom, Error, ErrorKind};
+use std::io::{Result, Error, ErrorKind, Seek, SeekFrom};
 use std::fmt;
-
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -22,13 +21,6 @@ impl Task {
     }
 }
 
-impl fmt::Display for Task {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
-        write!(f, "{:<50} [{}]", self.text, created_at)
-    }
-}
-
 fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
     file.seek(SeekFrom::Start(0))?; // Rewind the file before.
     let tasks = match serde_json::from_reader(file) {
@@ -38,6 +30,40 @@ fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
     };
     file.seek(SeekFrom::Start(0))?; // Rewind the file after.
     Ok(tasks)
+}
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+    let mut tasks = collect_tasks(&file)?;
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+    Ok(())
+}
+
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+    // Open the file.
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+
+    // Consume file's contents as a vector of tasks.
+    let mut tasks = collect_tasks(&file)?;
+
+    // Try to remove the task.
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
+    }
+    tasks.remove(task_position - 1);
+
+    // Write the modified task list back into the file.
+    file.set_len(0)?;
+    serde_json::to_writer(file, &tasks)?;
+    Ok(())
 }
 
 pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
@@ -60,41 +86,9 @@ pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
     Ok(())
 }
 
-pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
-    // Open the file.
-    let file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(journal_path)?;
-
-    let mut tasks = collect_tasks(&file)?;
-
-    // Remove the task.
-    if task_position == 0 || task_position > tasks.len() {
-        return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, created_at)
     }
-    tasks.remove(task_position - 1);
-
-    file.set_len(0)?;
-
-    // Write the modified task list back into the file.
-    serde_json::to_writer(file, &tasks)?;
-    Ok(())
 }
-
-pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
-    // Open the file.
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(journal_path)?;
-
-    let mut tasks = collect_tasks(&file)?;
-    tasks.push(task);
-
-    serde_json::to_writer(file, &tasks)?;
-
-    Ok(())
-}
-
